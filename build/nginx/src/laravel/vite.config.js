8bd10a67e1dda515cdaea290ac3ed59bb95e885a
Michael Schönwiese <michael.schoenwiese@loxone.com>
2024-09-10 11:52:22 +0200

    updated env

--- build/nginx/src/laravel/vite.config.js
+++ build/nginx/src/laravel/vite.config.js
@@ -1,24 +1,19 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
-server: {
+    server: {
       host: "0.0.0.0"
     },
     plugins: [
         laravel({
-            input: 'resources/js/app.js',
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
-        }),
-        vue({
-            template: {
-                transformAssetUrls: {
-                    base: null,
-                    includeAbsolute: false,
-                },
-            },
         }),
     ],
 });
+
 
